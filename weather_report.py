7a25022271c3bcb5135a0b19baeceecec26803a1
lisien520 <wwzhappy@outlook.jp>
2024-10-05 15:20:35 +0800

    Update weather_report.py

--- weather_report.py
+++ weather_report.py
@@ -1,16 +1,23 @@
+
 # 安装依赖 pip3 install requests html5lib bs4 schedule
-import os
+
+import time
 import requests
 import json
+import schedule
 from bs4 import BeautifulSoup
 
+
 # 从测试号信息获取
-appID = os.environ.get("APP_ID")
-appSecret = os.environ.get("APP_SECRET")
-# 收信人ID即 用户列表中的微信号
-openId = os.environ.get("OPEN_ID")
+appID = "wx47c08cb11f284ede"
+appSecret = "c65f9fe5c8de85ea463b79188cbac680"
+#收信人ID即 用户列表中的微信号，见上文
+openId = "opGTx6mq24Mi31tKJazCeA8Tudrg"
 # 天气预报模板ID
-weather_template_id = os.environ.get("TEMPLATE_ID")
+weather_template_id = "5vXR7I8WS63BjGBTKdpes71n_qB0OkaG7MjOTQYJwog"
+# 时间表模板ID
+timetable_template_id = ""
+
 
 def get_weather(my_city):
     urls = ["http://www.weather.com.cn/textFC/hb.shtml",
@@ -117,17 +124,38 @@ def send_weather(access_token, weather):
     print(requests.post(url, json.dumps(body)).text)
 
 
+def send_timetable(access_token, message):
+    body = {
+        "touser": openId,
+        "template_id": timetable_template_id.strip(),
+        "url": "https://weixin.qq.com",
+        "data": {
+            "message": {
+                "value": message
+            },
+        }
+    }
+    url = 'https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}'.format(access_token)
+    print(requests.post(url, json.dumps(body)).text)
 
-def weather_report(this_city):
+
+def weather_report(city):
     # 1.获取access_token
     access_token = get_access_token()
     # 2. 获取天气
-    weather = get_weather(this_city)
+    weather = get_weather(city)
     print(f"天气信息： {weather}")
     # 3. 发送消息
     send_weather(access_token, weather)
 
 
+def timetable(message):
+    # 1.获取access_token
+    access_token = get_access_token()
+    # 3. 发送消息
+    send_timetable(access_token, message)
+
 
 if __name__ == '__main__':
-    weather_report("淄博")
+    weather_report("济宁")
+    
